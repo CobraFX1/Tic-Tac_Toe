158ffdde87ba8e82ac3a3ba2b2c6b0c22e1dea41
Jacob Stephen <Cashflow34steph@gmail.com>
2026-01-14 10:06:40 +0100

    Implemented the placeMark function for the gameBoard

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-let gameBoard = [];
-let gameBoardObj = function () {
-    let gameBoard = [];
-};
-let player = function (name) {
-    const playerName = name;
-    let moves = [];
-    return function (move) {
-        moves.push(move);
-        gameBoard.push(move);
-        return { playerName, moves }
-    }
-};
-let gameLogic = function () {
-    const player1 = player("stephen");
-    const player2 = player("james");
-};
-const player1 = player("stephen");
-    const player2 = player("james");
-gameLogic();
+const Gameboard = (function () {
+    let board = ["", "", "", "", "", "", "", "", ""];
+
+    return {
+        getBoard: () => board,
+        placeMark: (position, mark) => {
+            (board[position] === false) ? board[position] = mark : "This position is already taken";
+        }
+    };
+})();
