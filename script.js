8876c869f8c63d7411ecad676b12838f2761a905
Jacob Stephen <Cashflow34steph@gmail.com>
2026-01-14 22:01:26 +0100

    Completed the logic for the tic tac toe console game

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
 const Gameboard = (function () {
     let board = ["", "", "", "", "", "", "", "", ""];
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]];
+    // Function to check for a win
+    let checkWinner = (mark) => {
+        for (const condition of winningConditions) {
+            const [num1, num2, num3] = condition;
+            if (board[num1] == mark && board[num2] == mark && board[num3] == mark) {
+                return true;
+            }
+        }
+    }
+    let checkTie = () => {
+        let spotsLeft = board.filter((item) => item == "");
+        if (spotsLeft.length == 0) {
 
+            return true;
+        }
+    }
+    let resetBoard = () => {
+        board = ["", "", "", "", "", "", "", "", ""];
+    }
     return {
         getBoard: () => board,
         placeMark: (position, mark) => {
-            (board[position] === '') 
-            ? board[position] = mark
-            : console.log("This position is already taken");
-        }
+            if (board[position] === '') {
+                board[position] = mark;
+                return true
+            }
+            else {
+                return false;
+            }
+        },
+        checkWinner,
+        checkTie,
+        resetBoard,
     };
 })();
 const Player = (name, mark) => {
-   
     return { name, mark };
 };
 const GameController = (function () {
+    let gameOver = false;
     const players = [
         Player("Stephen", "X"),
         Player("James", "O")
@@ -25,8 +59,33 @@ const GameController = (function () {
     };
     return {
         playRound: (position) => {
-            Gameboard.placeMark(position, activePlayer.mark);
-            switchPlayer();
-        }
+            if (gameOver == true) {
+                gameOver = false;
+                activePlayer = players[0];  
+                Gameboard.resetBoard();
+            };
+            if ((Gameboard.placeMark(position, activePlayer.mark)) === true) {
+                console.log(Gameboard.getBoard())
+                const win = (Gameboard.checkWinner(activePlayer.mark) == true);
+                if (win || (Gameboard.checkTie() == true)) {
+                    if (win) {
+                        gameOver = true;
+                        console.log(`${activePlayer.name} is the winner!`);
+                        return;
+                    } else {
+                        gameOver = true;
+                        console.log("This round is a draw");
+                        return;
+                    }
+                } else {
+                    switchPlayer();
+                }
+            } else {
+                console.log("This position is already taken");
+                return;
+            }
+            // console.log(Gameboard.getBoard())
+        },
+        getActivePlayer: () => activePlayer,
     };
 })();
