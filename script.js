183977b276f1407f4d8ae8e9eeb7cedd62424832
Jacob Stephen <Cashflow34steph@gmail.com>
2026-01-14 10:24:03 +0100

    Implemented part of the GameController Logic

--- script.js
+++ script.js
@@ -4,7 +4,29 @@ const Gameboard = (function () {
     return {
         getBoard: () => board,
         placeMark: (position, mark) => {
-            (board[position] === false) ? board[position] = mark : "This position is already taken";
+            (board[position] === '') 
+            ? board[position] = mark
+            : console.log("This position is already taken");
+        }
+    };
+})();
+const Player = (name, mark) => {
+   
+    return { name, mark };
+};
+const GameController = (function () {
+    const players = [
+        Player("Stephen", "X"),
+        Player("James", "O")
+    ];
+    let activePlayer = players[0];
+    const switchPlayer = () => {
+        activePlayer = (activePlayer === players[0]) ? players[1] : players[0];
+    };
+    return {
+        playRound: (position) => {
+            Gameboard.placeMark(position, activePlayer.mark);
+            switchPlayer();
         }
     };
 })();
